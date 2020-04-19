47494d52df339f53f009fcf2cee74892b0383bcc
Dang <dangtcm2@gmail.com>
2020-04-19 20:06:40 +0700

    Removed all file

--- 2018/TNIY85_FASTLED_BORDER1/Sweep.ino
+++
@@ -1,9 +0,0 @@
-void rainbowSweep() {
-
-  for (int i = 0; i < NUM_STRINGS; i++) {
-    shiftOut16(1 << i);
-    fill_solid(leds, NUM_LEDS, CHSV(gHue + 18 * i, 255, 255));
-    FastLED.show();
-  }
-}
-
